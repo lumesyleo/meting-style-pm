f0b7bb8786e218e54bfbae2ea82e8db7ea69c74f
Airh <AlyQWERTY@163.com>
2026-04-17 12:40:08 +0800

    first commit

--- test/old-ver/assets/_dir/data/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:14:{s:14:"admin_username";s:5:"admin";s:14:"admin_password";s:32:"e10adc3949ba59abbe56e057f20f883e";s:5:"title";s:18:"彩虹目录列表";s:8:"keywords";s:60:"彩虹目录列表,Directory Lister目录列表,目录索引";s:11:"description";s:24:"彩虹目录列表程序";s:8:"announce";s:0:"";s:6:"footer";s:0:"";s:11:"name_encode";s:4:"utf8";s:9:"file_hash";s:1:"1";s:13:"cache_indexes";s:1:"0";s:10:"footer_bar";s:1:"1";s:9:"readme_md";s:1:"1";s:4:"auth";s:1:"0";s:3:"nav";s:101:"音乐搜索*http://music.hi.cn/|图片压缩*https://tinypng.com/|今日热榜*https://tophub.today/";}
